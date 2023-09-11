e60090979b6dd6597ed3aa92050a9fc4bfbf78c7
leandroftassara <leandroftassara@gmail.com>
2023-09-11 10:41:32 -0300

    feat: add Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,29 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Home - Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+   return (
+      <div className="">
+         <Head>
+            <title>Home - Netflix</title>
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
 
-      {/* Header */}
+         <Header />
 
-      <main>
-        {/* Banner */}
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-        </section>
-      </main>
+         <main>
+            {/* Banner */}
+            <section>
+               {/* Row */}
+               {/* Row */}
+               {/* Row */}
+            </section>
+         </main>
 
-      {/* Modal */}
-    </div>
-  );
+         {/* Modal */}
+      </div>
+   );
 };
 
 export default Home;
