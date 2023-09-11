2d41f4b3748c7ef04073e51a882ff9114fef7dad
leandroftassara <leandroftassara@gmail.com>
2023-09-11 10:46:19 -0300

    style: update main div

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,27 @@ import Head from 'next/head';
 import Header from '../components/Header';
 
 const Home: NextPage = () => {
-   return (
-      <div className="">
-         <Head>
-            <title>Home - Netflix</title>
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
+  return (
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-         <Header />
+      <Header />
 
-         <main>
-            {/* Banner */}
-            <section>
-               {/* Row */}
-               {/* Row */}
-               {/* Row */}
-            </section>
-         </main>
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
 
-         {/* Modal */}
-      </div>
-   );
+      {/* Modal */}
+    </div>
+  );
 };
 
 export default Home;
